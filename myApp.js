8f6f9a712828c0163fc5aae8e8899558f44c4730
philip-ifeanyi <philipifeanyi2000@gmail.com>
2023-11-05 23:01:08 +0100

    done

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true});
 
 const personSchema = new mongoose.Schema({
   name: {
