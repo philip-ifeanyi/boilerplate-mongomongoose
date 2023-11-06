752d93a7f91e5346a4ec2571bcf0feec461b7e43
philip-ifeanyi <philipifeanyi2000@gmail.com>
2023-11-06 14:49:34 +0100

    done

--- myApp.js
+++ myApp.js
@@ -109,7 +109,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({id: personId}, (err, data) => {
+    if (err) return console.err(err)
+    done(null, data);
+  })
 };
 
 const removeManyPeople = (done) => {
